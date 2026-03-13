74cbeeb986ab899cc82c895ba2f46612fca0d1df
Cadu-Menezes <cadumenezes051@gmail.com>
2026-03-13 16:58:54 -0300

    .

--- App.java
+++ App.java
@@ -5,9 +5,9 @@
 
 public class App {
     public static void main(String[] args) {
-        Order order = new Order("João", "joao@email.com", 0.1);
-        order.addItem(new OrderItem("Notebook", 1, 3500.0));
-        order.addItem(new OrderItem("Mouse", 2, 80.0));
+        Order order = new Order("Cadu", "carlos.alencar@al.infnet.edu.br", 0.1);
+        order.addItem(new OrderItem("Memoria RAM 32BG DDR5", 1, 3500.0));
+        order.addItem(new OrderItem("Placa de Video RTX 5070 Ti", 2, 8000.0));
         order.printInvoice();
         order.sendEmail();
     }
